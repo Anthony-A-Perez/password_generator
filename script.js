8f3081c9723d1da801da4d7546e73a0422c794c6
Anthony-A-Perez <ap.erez@me.com>
2023-02-12 15:23:04 -0800

    comments update

--- script.js
+++ script.js
@@ -106,36 +106,3 @@ function generatePassword() {
   }
 };
 
-// Get references to the #generate element
-//var generateBtn = document.querySelector("#generate");
-
-
-
-
- 
-
-// Write password to the #password input
-
-
-
-//function writePassword() {
- // var password = generatePassword();
- // var passwordText = document.querySelector("#password");
-
-//  passwordText.value = password;
-
-//}
-
-// Add event listener to generate button
-
-
-//generateBtn.addEventListener("click", writePassword);
-
-
-
-// 1. prompt the user
-  // a. PW length btwn 8 and 128
-  // b. lowercase, uppercase, numbers, special characters
-  // 2. validate input
-  // 3. generate
-  // 4. display PW
