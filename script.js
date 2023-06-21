39c96cabd871137e49eae6d45ac5988912dc7da6
Anthony-A-Perez <ap.erez@me.com>
2023-06-20 21:02:55 -0700

    refactored code

--- script.js
+++ script.js
@@ -1,108 +1,86 @@
-// Assignment code here
+// Define arrays for characters, numbers, and letters
+var character = ['!', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '<', '>', '+', '=', '@', '{', '}', '~', '.'];
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var space = [];
 
-var enter;
+// Variables to store user choices
 var confirmNumber;
 var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
 
-character = ['!', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '<', '>', '+', '=', '@', '{', '}', '~', '.'];
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-space = [];
-
-var choices;
-
+// Function to convert letter to uppercase
 var toUpper = function (x) {
-  return x.toUpperCase();
+    return x.toUpperCase();
 };
 
-lettersUp = letters.map(toUpper);
+// Map letters array to uppercase letters
+var lettersUp = letters.map(toUpper);
 
+// Get generate button element
 var get = document.querySelector('#generate');
 
-get. addEventListener('click', function () {
-  pw = generatePassword();
-  document.getElementById('password').placeholder = pw;
+// Add event listener to generate button
+get.addEventListener('click', function () {
+    var pw = generatePassword();
+    document.getElementById('password').placeholder = pw;
 });
 
+// Function to generate password
 function generatePassword() {
-  enter = parseInt(prompt('How many characters would you like your password? Choose between 8 and 128'));
+    // Get user input for password length
+    var enter = parseInt(prompt('How many characters would you like your password? Choose between 8 and 128'));
 
-  if (!enter) {
-    alert('This needs a value');
-  } else if (enter < 8 || enter > 128) {
-    enter = parseInt(prompt('You must choose between 8 and 128'));
+    // Validate user input for password length
+    while (!enter || enter < 8 || enter > 128) {
+        enter = parseInt(prompt('You must choose between 8 and 128'));
+    }
 
-  } else {
+    // Get user input for password criteria
     confirmNumber = confirm('Will this contain numbers?');
     confirmCharacter = confirm('Will this contain special characters?');
     confirmUppercase = confirm('Will this contain Uppercase letters?');
     confirmLowercase = confirm('Will this contain Lowercase letters?');
-  };
 
-  if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
-    choices = alert('You must choose a criteria!');
-  }
+    // Validate user input for password criteria
+    while (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+        alert('You must choose at least one criteria!');
+        confirmNumber = confirm('Will this contain numbers?');
+        confirmCharacter = confirm('Will this contain special characters?');
+        confirmUppercase = confirm('Will this contain Uppercase letters?');
+        confirmLowercase = confirm('Will this contain Lowercase letters?');
+    }
 
-  else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
-    choices = character.concat(number, letters, lettersUp);
-  }
-  else if (confirmCharacter && confirmNumber && confirmUppercase) {
-    choices = character.concat(number, lettersUp);
-  }
-  else if (confirmCharacter && confirmNumber && confirmLowercase) {
-    choices = character.concat(number, letters);
-  }
-  else if (confirmCharacter && confirmLowercase && confirmUppercase) {
-    choices = character.concat(letters, lettersUp);
-  }
-  else if (confirmNumber &&  confirmLowercase && confirmUppercase) {
-    choices = number.concat(letters, lettersUp);
-  }
-  else if (confirmCharacter && confirmNumber) {
-    choices = character.concat(number);
-  }
-  else if (confirmCharacter && confirmLowercase) {
-    choices = character.concat(letters);
-  }
-  else if (confirmCharacter && confirmUppercase) {
-    choices = character.concat(lettersUp);
-  } 
-  else if (confirmLowercase && confirmNumber) {
-    choices = letters.concat(number);
-  }
-  else if (confirmLowercase && confirmUppercase) {
-    choices = letters.concat(lettersUp);
-  }
-  else if (confirmNumber && confirmUppercase) {
-    choices = number.concat(lettersUp);
-  }
-  else if (confirmCharacter) {
-    choices = character;
-  }
-  else if (confirmNumber) {
-    choices = number;
-  }
-  else if (confirmLowercase) {
-    choices = letters;
-  }
-  else if (confirmUppercase) {
-    choices = space.concat(lettersUp);
-  };
+    // Concatenate arrays based on user choices
+    var choices = [];
+    if (confirmCharacter) {
+        choices = choices.concat(character);
+    }
+    if (confirmNumber) {
+        choices = choices.concat(number);
+    }
+    if (confirmLowercase) {
+        choices = choices.concat(letters);
+    }
+    if (confirmUppercase) {
+        choices = choices.concat(lettersUp);
+    }
 
-  var password = [];
+    // Generate password by randomly selecting characters from choices array
+    var password = [];
+    for (var i = 0; i < enter; i++) {
+        var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+        password.push(pickChoices);
+    }
 
-  for (var i = 0; i < enter; i++) {
-    var pickChoices = choices[Math.floor(Math.random() * choices.length)];
-    password.push(pickChoices);
-  }
-  var pw = password.join('');
-  UserInput(pw);
-  return pw;
+    // Convert password array to string and display in password element
+    var pw = password.join('');
+    UserInput(pw);
+    return pw;
+}
 
-  function UserInput(pw) {
+// Function to update password element with generated password
+function UserInput(pw) {
     document.getElementById('password').textContent = pw;
-  }
-};
-
+}
